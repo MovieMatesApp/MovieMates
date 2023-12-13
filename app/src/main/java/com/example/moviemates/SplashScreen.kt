0f6deac02ed00f286b8866a7f3898123068952c1
SumitSerawt <ss4366@njit.edu>
2023-12-14 03:41:25 +0530

    code update

--- app/src/main/java/com/example/moviemates/SplashScreen.kt
+++ app/src/main/java/com/example/moviemates/SplashScreen.kt
@@ -1,11 +1,21 @@
 package com.example.moviemates
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this@SplashScreen, SignInActivity::class.java))
+            finish() // close the splash screen to prevent going back
+        }, 2000)
+
+
     }
 }
